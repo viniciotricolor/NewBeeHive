9ebeb831e0fe500cf9eb98e78b814453455775ca
[dyad] <viniciotricolorsaopaulo@gmail.com>
2025-09-21 23:13:28 -0300

    [dyad] Removendo arquivos CSS desnecessários - wrote 1 file(s), deleted 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+// O arquivo App.css foi removido, então a importação também é removida.
 
 createRoot(document.getElementById("root")!).render(<App />);
